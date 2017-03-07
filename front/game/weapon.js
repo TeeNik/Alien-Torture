e07d12d56b6dcf6911f5d1311ea357a7e975101f
TeeNik <teenik3315@gmail.com>
2017-03-07 14:01:31 +0300

    validation

--- front/game/weapon.js
+++ front/game/weapon.js
@@ -1,113 +1,105 @@
 var bulls = [];
-
 obj.fireCheck = true;
-
-var createShot = function(fileName = "assets/pistolShot.png", sp = 10, dm = 1){
-    var bull = game.newImageObject( { 
-      file : fileName, 
-      x: oPos.x,
-      y: oPos.y, 
-      scale: 0.35,
-      angle: shotPoint.getAngle(),
-      userData: {
-         life: 1
-      }
+var createShot = function (fileName = "assets/pistolShot.png", sp = 10, dm = 1) {
+    var bull = game.newImageObject({
+        file: fileName,
+        x: oPos.x,
+        y: oPos.y,
+        scale: 0.35, 
+        angle: shotPoint.getAngle(),
+        userData: {
+            life: 1
+        }
     });
     bull.damage = dm;
     bull.speed = sp;
     bulls.push(bull);
 }
-
 obj.weapon = weapons[0];
-
-
-var pistolShot = function(){
-    if(obj.fireCheck){
+var pistolShot = function () {
+    if (obj.fireCheck) {
         obj.fireCheck = false;
         createShot();
-        setTimeout(function(){
-           obj.fireCheck = true; 
+        setTimeout(function () {
+            obj.fireCheck = true;
         }, 500);
     }
 };
-
-var assaultShot = function(){
-    if(obj.fireCheck){
+var assaultShot = function () {
+    if (obj.fireCheck) {
         obj.fireCheck = false;
-        let  i = 3;
-        setTimeout(function rifle(){
-            if(i > 0){         
+        let i = 3;
+        setTimeout(function rifle() {
+            if (i > 0) {
                 createShot();
                 i--;
                 setTimeout(rifle, 100);
             }
-        }, 100); 
-        setTimeout(function(){
-           obj.fireCheck = true; 
+        }, 100);
+        setTimeout(function () {
+            obj.fireCheck = true;
         }, 500);
     }
 };
-
-var gunShot = function(){
-    if(obj.fireCheck){
+var gunShot = function () {
+    if (obj.fireCheck) {
         obj.fireCheck = false;
-        let  i = 2;
-        setTimeout(function rifle(){
-            if(i > 0){         
+        let i = 2;
+        setTimeout(function rifle() {
+            if (i > 0) {
                 createShot("assets/gunShot.png", 10, 1.5);
                 i--;
                 setTimeout(rifle, 100);
             }
-        }, 100); 
-        setTimeout(function(){
-           obj.fireCheck = true; 
+        }, 100);
+        setTimeout(function () {
+            obj.fireCheck = true;
         }, 500);
     }
 };
-
-var sniperShot = function(){
-    if(obj.fireCheck){
+var sniperShot = function () {
+    if (obj.fireCheck) {
         obj.fireCheck = false;
         createShot("assets/soran.png", 20, 1.5);
-        setTimeout(function(){
-           obj.fireCheck = true; 
+        setTimeout(function () {
+            obj.fireCheck = true;
         }, 500);
     }
 };
-
-var plasmaShot = function(){
-    if(obj.fireCheck){
+var plasmaShot = function () {
+    if (obj.fireCheck) {
         obj.fireCheck = false;
         createShot("assets/plasmaShot.png", 20, 2);
-        setTimeout(function(){
-           obj.fireCheck = true; 
+        setTimeout(function () {
+            obj.fireCheck = true;
         }, 500);
     }
 };
-
-obj.moveWeapon = function(){
-     obj.weapon.move(point(obj.dx,obj.dy)); 
-    
+obj.moveWeapon = function () {
+    obj.weapon.move(point(obj.dx, obj.dy));
     obj.weapon.rotate(mouse.getPosition());
-    
-    if(obj.addSpec){
-        obj.addSpec.move(point(obj.dx,obj.dy));
+    if (obj.addSpec) {
+        obj.addSpec.move(point(obj.dx, obj.dy));
     }
 }
-
 var fire = function () {
     if (mouse.isPress("LEFT")) {
-        switch(obj.wNum % 5){
-            case 0: pistolShot();
-                break;
-            case 1: assaultShot();
-                break;
-            case 2: sniperShot();
-                break;
-            case 3: gunShot();
-                break;
-            case 4: plasmaShot();
-                break;
+        switch (obj.wNum % 5) {
+        case 0:
+            pistolShot();
+            break;
+        case 1:
+            assaultShot();
+            break;
+        case 2:
+            sniperShot();
+            break;
+        case 3:
+            gunShot();
+            break;
+        case 4:
+            plasmaShot();
+            break;
         }
         assaultShot();
     }
@@ -118,7 +110,7 @@ var fire = function () {
             if (el.isIntersect(wall)) {
                 el.visible = false;
             }
-            if(el.isIntersect(enemy) && el.isVisible() && enemy.isVisible()) {
+            if (el.isIntersect(enemy) && el.isVisible() && enemy.isVisible()) {
                 el.visible = false;
                 enemy.health -= el.damage;
             }
